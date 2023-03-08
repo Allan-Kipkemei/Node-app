04c0e9aebcd1874af2b9485e02cc25b1b554399e
Allan Kiplagat <allankiplgat@gmail.com>
2023-03-08 09:44:37 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+//load the env variables
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+
+//import dependencies
+const express = require('express');
+const conectToDb = require('./config/connectToDb')
+const Note = require('./models/Note');
+//create an express app
+const app = express();
+app.use(express.json());
+//connect to database
+conectToDb();
+//routing
+app.get('/', (req, res) => {
+    res.json({ hello: 'world}' });
+
+})
+app.post('/Note', async (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const note = await Note.create({
+        title: title,
+        body: body,
+    })
+    res.json({
+        note: note
+    })
+})
+//start the server
+
+app.listen(process.env.PORT);
